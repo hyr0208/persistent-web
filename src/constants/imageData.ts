01401ffb07b3f1d0e76a659e11238af40af000de
hyr0208 <hyr0208@naver.com>
2024-01-17 16:26:41 +0900

    feat: 메인페이지 구현중

---
+++ src/constants/imageData.ts
@@ -0,0 +1,4 @@
+export const imageData = {
+  logo: require("../assets/images/logo.png"),
+  banner: require("../assets/images/banner.png"),
+};
--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { imageData } from "../../constants/imageData";
 
 function MainPage() {
-  return <div>MainPage</div>;
+  return (
+    <div className="overflow-hidden">
+      <div className="max-w-[1920px] m-auto ">
+        <div className="relative">
+          <img src={imageData.banner} alt="" className="w-[1920px] h-[300px]" />
+        </div>
+        <div className="flex justify-center mt-[97px]">
+          <p className="font-bold text-[40px]">기업소개</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
