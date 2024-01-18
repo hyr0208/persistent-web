1ffe1108ddce02be9bbefc5dbd72cb45a8cb240c
hyr0208 <hyr0208@naver.com>
2024-01-18 11:01:14 +0900

    feat: 텍스트 수정

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -41,11 +41,9 @@ function MainPage() {
           </div>
           <div className="flex justify-center text-center mb-[140px] mx-6">
             <p className="text-[18px]">
-              집요한사람들은, 우리나라에서 고급도시락의 시장을 열었다는 평을
-              듣는
+              우리의 고객 뿐 아니라 집요한사람들의 모든 구성원들이
               <br />
-              고급도시락 브랜드의 원조 "옹가솜씨"를 만든 옹수민 셰프가 설립한
-              기업입니다.
+              행복할 수 있는 길을 찾아가는 여정에서, 가치를 추구하는 조직
             </p>
           </div>
           <div className="flex flex-row justify-center gap-[100px] mx-6 ">
@@ -57,9 +55,9 @@ function MainPage() {
               />
 
               <p className="text-center">
-                음식 뿐아니라 경험에도 큰 가치를 갖게 함으로서
+                스타가 되려는 노력보다 팀웤으로 고성과를 내는 아름다운 모습
                 <br />
-                일상에서 행복지수가 높아 지는것을 돕는것이 우리의 비젼
+                열심히 일한만큼 성과로 당당히 평가받는 조직
               </p>
             </div>
 
