e174289e60cf62bd3e6535bb39afa1804a2ca4a6
yyyerin <hyr0208@naver.com>
2024-09-03 10:52:18 +0900

    chore: setting

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import BrandIntroduction from "../pages/brand/BrandIntroduction";
 import CompanyIntroduction from "../pages/\bcompany/\bCompanyIntroduction";
 
 function Router() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<CompanyIntroduction />}></Route>
-        <Route
-          path="/brandintroduction"
-          element={<BrandIntroduction />}
-        ></Route>
-      </Route>
-    </Routes>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<CompanyIntroduction />}></Route>
+          <Route
+            path="/brandintroduction"
+            element={<BrandIntroduction />}
+          ></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
