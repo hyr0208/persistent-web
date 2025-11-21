28e181c380b7d28ea22626a3e8d93f63d700ec5c
yyyerin <hyr0208@naver.com>
2025-11-21 15:04:15 +0900

    refactor: 경로 수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,20 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import BrandIntroduction from "../pages/brand/BrandIntroduction";
-import CompanyIntroduction from "../pages/\bcompany/\bCompanyIntroduction";
+import CompanyIntroduction from "../pages/company/CompanyIntroduction";
 
 function Router() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<CompanyIntroduction />}></Route>
-          <Route
-            path="/brandintroduction"
-            element={<BrandIntroduction />}
-          ></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<CompanyIntroduction />}></Route>
+        <Route
+          path="/brandintroduction"
+          element={<BrandIntroduction />}
+        ></Route>
+      </Route>
+    </Routes>
   );
 }
 
